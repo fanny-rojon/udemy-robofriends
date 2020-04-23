d049293ce94be59d3fd9c38a3a98a695ef8460e3
Fanny Rojon <fanny.rojon22@gmail.com>
2020-04-23 12:21:12 +0200

    React exercise

---
+++ src/CardList.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Card from './Card';
+
+const CardList = ({ robots }) => {
+  const cardsArray = robots.map((user, index) => {
+    return <Card id={robots[index].id} name={robots[index].name} email={robots[index].email}/>
+  })
+  return (
+    <div>
+      {cardsArray}
+    </div>
+  );
+}
+
+export default CardList;
